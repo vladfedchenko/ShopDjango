40594e7ed221240800205d5d93a0707c4e380232
Vladyslav Fedchenko <vladfedchenko@gmail.com>
2016-11-22 23:34:36 +0200

    Model development

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+admin.site.register(UniqueID)
+admin.site.register(ItemType)
+admin.site.register(Brand)
+admin.site.register(TV)
+
--- shopapp/models.py
+++ shopapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class ItemType(models.Model):
+	name = models.CharField(max_length=200)
+	
+	def __str__(self):
+		return str(self.id) + ". " + self.name
+
+class UniqueID(models.Model):
+	item_type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
+	
+	def __str__(self):
+		return str(self.id) + ". " + str(self.item_type)
+	
+class Brand(models.Model):
+	name = models.CharField(max_length=200)
+	
+	def __str__(self):
+		return str(self.id) + ". " + self.name
+	
+class ShopItem(models.Model):
+	brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+	model = models.CharField(max_length=200)
+	unique_id = models.ForeignKey(UniqueID, on_delete=models.CASCADE)
+	price = models.FloatField()
+	
+	def __str__(self):
+		return str(self.id) + ". " + str(self.brand) + ', ' + str(self.model)
+	
+	class Meta:
+		abstract = True
+	
+	
+class TV(ShopItem):
+	diagonal = models.FloatField()
+	is_smart = models.BooleanField()
+	
+	
