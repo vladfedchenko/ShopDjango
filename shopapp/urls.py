914453ac93beacb92afbbc218b38176c6886e830
Vladyslav Fedchenko <vladfedchenko@gmail.com>
2016-11-23 23:15:48 +0200

    Views development

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'shopapp'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^(?P<category_val>[a-z]+)/$', views.category, name='category'),
 ]
--- shopapp/views.py
+++ shopapp/views.py
@@ -1,4 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import *
+
+categories = {'tv' : ('TVs', TV),
+				'monitor' : ('Monitors', Monitor),
+				'projector' : ('Projectors', Projector)}
 
 def index(request):
-	return HttpResponse("shopapp index")
+	context = {'categories' : categories}
+	return render(request, 'shopapp/categories.html', context)
+	
+def category(request, category_val):
+	cls = categories[category_val][1]
+	#tv = cls.objects.get(id=1)
+	return HttpResponse(category_val)
